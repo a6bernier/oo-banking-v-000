110decb566b68f52631e7f6f3d808ace8a55c8f1
Andrew Bernier <a6bernier@gmail.com>
2018-10-19 18:28:53 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,7 +30,7 @@ class Transfer
 
   def reverse_transfer
     if @status == "complete"
-      @sender.balance += @amount
+      @sender += @amount
       @receiver.balance -= @amount
       @status = "reversed"
     end
